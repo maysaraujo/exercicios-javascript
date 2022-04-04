53bea1b9a0b8ecd1a84d37df54a202d22cdeb1b4
maysaraujo <maysaaraujo25@gmail.com>
2022-04-04 18:26:44 -0300

    adicionado ex14

--- ex14/ex14.js
+++ ex14/ex14.js
@@ -1,3 +1,4 @@
+//Crie uma função que recebe dois parâmetros. base e expoente, e retorne a base elevada ao expoente.
 function potencia(base, expoente) {
   console.log(Math.pow(base, expoente));
 }
