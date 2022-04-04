3635b57f5d05b1797fdf1a5d0e6547448b504d70
maysaraujo <maysaaraujo25@gmail.com>
2022-04-04 18:28:06 -0300

    adicionado ex15

--- ex15/ex15.js
+++ ex15/ex15.js
@@ -1,3 +1,5 @@
+//Crie uma função que irá receber dois valores, o dividendo e o divisor.
+//A função deverá imprimir o resultado e o resto da divisão destes dois valores.
 function divisao(dividendo, divisor) {
   let dividir = dividendo / divisor;
   let resto = dividendo % divisor;
