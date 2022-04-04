afea4ccf728d6f8f07b51b5aebb85d12f9fceba0
maysaraujo <maysaaraujo25@gmail.com>
2022-04-04 18:02:04 -0300

    atualizado

--- ex12/ex12.js
+++ ex12/ex12.js
@@ -1,21 +1,21 @@
 //Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma,
 //subtração, multiplicação e divisão desses valores.
 
-const somar = function (n1 = 0, n2 = 0) {
+function somar(n1 = 0, n2 = 0) {
   console.log(`O resultado da soma é: ${n1 + n2}`);
-};
+}
 
-const subtrair = function (n1 = 0, n2 = 0) {
+function subtrair(n1 = 0, n2 = 0) {
   console.log(`O resultado da subtração é: ${n1 - n2}`);
-};
+}
 
-const multiplicar = function (n1 = 0, n2 = 0) {
+function multiplicar(n1 = 0, n2 = 0) {
   console.log(`O resultado da multiplicação é: ${n1 * n2}`);
-};
+}
 
-const dividir = function (n1 = 0, n2 = 0) {
+function dividir(n1 = 0, n2 = 0) {
   console.log(`O resultado da divisão é: ${n1 / n2}`);
-};
+}
 
 somar(10, 8);
 
