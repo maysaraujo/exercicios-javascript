bccaa17d26a6cc747deedc62fd0e201ce214fede
maysaraujo <maysaaraujo25@gmail.com>
2022-04-04 18:23:34 -0300

    adicionado ex15

---
+++ ex15/ex15.js
@@ -0,0 +1,9 @@
+function divisao(dividendo, divisor) {
+  let dividir = dividendo / divisor;
+  let resto = dividendo % divisor;
+
+  console.log(`Divisão: ${dividir}`);
+  console.log(`Resto: ${resto}`);
+}
+
+divisao(12, 4);
