d29d33927322590b537db854a19de2aff6c8b6c5
maysaraujo <maysaaraujo25@gmail.com>
2022-04-13 09:50:21 -0300

    adicionado ex10

---
+++ node/ex10.js
@@ -0,0 +1,12 @@
+function anoBissexto(ano) {
+  if (ano % 4 == 0) {
+    console.log("É bissexto");
+  } else {
+    console.log("Não é bissexto");
+  }
+}
+
+anoBissexto(2020);
+anoBissexto(2003);
+anoBissexto(2011);
+anoBissexto(2016);
