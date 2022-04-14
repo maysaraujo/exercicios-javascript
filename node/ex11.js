b8fdadf699e0b6357129976ca438b2e81f842640
maysaraujo <maysaaraujo25@gmail.com>
2022-04-14 09:53:24 -0300

    adicionado ex11

--- node/ex11.js
+++ node/ex11.js
@@ -1,5 +1,11 @@
-function fatorial(numero = 10) {
-  for (let i = 0; i < numero; i++) {
-    console.log(i);
+/*Faça um algoritmo que calcule o fatorial de um número.*/
+
+function fatorial(numero) {
+  if (numero == 1) {
+    return 1;
   }
+
+  return numero * fatorial(numero - 1);
 }
+
+console.log(fatorial(5));
