9948b2468a7ac6391d226289d8a49e129a3d061d
Maysa Araujo <80985594+maysaraujo@users.noreply.github.com>
2022-06-11 15:26:13 -0300

    Delete DOM directory

--- DOM/ex03/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function clicar() {
-  let numero = document.getElementById("btn");
-  numero = parseInt(prompt("Insira um número inteiro qualquer: "));
-  alert(`Antes de ${numero} temos ${numero - 1}`);
-  alert(`Depois de ${numero} temos ${numero + 1}`);
-}
