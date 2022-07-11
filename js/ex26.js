9917c5ae48e72af32ab773c67c31fedd819962bb
maysaraujo <maysaaraujo25@gmail.com>
2022-07-11 15:46:31 -0300

    add ex26

---
+++ js/ex26.js
@@ -0,0 +1,16 @@
+/* Verifique, de duas maneiras diferentes entre si, se uma String é um palíndromo.
+
+Palíndromo: frase ou palavra que se pode ler, 
+indiferentemente, da esquerda para a direita ou vice-versa 
+(ex.: raiar, ama, ovo, radar) */
+
+function palindromo(string) {
+  console.log(
+    `${string} é um palíndromo?\n${
+      string === string.split("").reverse().join("")
+    }`
+  );
+}
+
+palindromo("asa");
+palindromo("abacaxi");
