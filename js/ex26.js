63ede1a2d00fe01bd7fb6143dd7273a56e30592f
maysaraujo <maysaaraujo25@gmail.com>
2022-07-11 15:47:25 -0300

    add ex26

--- js/ex26.js
+++ js/ex26.js
@@ -1,5 +1,4 @@
 /* Verifique, de duas maneiras diferentes entre si, se uma String é um palíndromo.
-
 Palíndromo: frase ou palavra que se pode ler, 
 indiferentemente, da esquerda para a direita ou vice-versa 
 (ex.: raiar, ama, ovo, radar) */
